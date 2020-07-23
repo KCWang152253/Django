bf339112a3b6e035c29097676ca4c5d727b893ca
KCWang152253 <2747962529@qq.com>
2020-07-23 19:30:31 +1000

    爱鲜蜂项目第2次提交

--- axf/project/project/settings.py
+++ axf/project/project/settings.py
@@ -74,15 +74,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "axf",
-        'USER': 'root',
-        'PASSWORD': 'wangcheng152253',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
